5e860bc93524d336c406c1d80a2c5049011b3a59
JWTR <joy.watiri@strathmore.edu>
2025-06-09 13:59:34 +0300

    Added web_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
 st.title("Title: Bio Data")
-st.write("This is a sample web app."
-)
+st.write("This is a sample web app.")
 
 first_name = st.text_input("First Name")
 last_name = st.text_input("Last Name")
 gender = st.selectbox("Gender",["Male","Female"])
-age = st.number_input("Your Age",0,100,30,1)
+age = st.number_input("Your Age", 0, 100, 30, 1)
 dob = st.date_input("Your Birthday")
 marital_status = st.radio("Marital Status",["Single","Married"])
-years_of_experience = st.slider("Years of experience",0,40)
+years_of_experience = st.slider("Years of experience", 0, 40)
